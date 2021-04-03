fb79651c67d59ce5a57301f86275ebad7bd2cdc4
StanislavaMiteva <stanislava.miteva@gmail.com>
2021-04-04 00:42:02 +0300

    Add Header and Footer.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer=() => {
+    return (
+        <footer className="site-footer">
+            <p>&copy; On-line Invoicing 2021</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="site-header">
+            <nav className="header-navbar">
+                <section className="navbar-user">
+                    <article className="first-bar">
+                        <Link to="/">On-line Invoicing</Link>
+                        <Link className="button" to="/add">New Invoice</Link>
+                        <Link className="button" to="/all">All Invoices</Link>
+                    </article>
+                    <article className="second-bar">
+                        <ul>
+                            <li>Welcome, some name!</li>
+                            <li>
+                                <Link to="/logout">
+                                    <i className="fas fa-sign-out-alt"></i>
+                                     Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </article>
+                </section>
+                <section className="navbar-anonymous">
+                    <ul>
+                        <li>
+                            <Link to="/register"><i className="fas fa-user-plus"></i> Register</Link>
+                        </li>
+                        <li>
+                            <Link to="/login"><i className="fas fa-sign-in-alt"></i> Login</Link>
+                        </li>
+                    </ul>
+                </section>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
